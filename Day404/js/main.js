ac037fe57e5c463898c9d70637f6d1f5777d21ef
WebcrazyDylan <webcrazy@naver.com>
2021-01-29 11:21:06 -0800

    update

--- Day404/js/main.js
+++ Day404/js/main.js
@@ -1,22 +1,29 @@
 let galleryImages = document.querySelectorAll(".galleryImages");
+// console.log(galleryImages);
 
 let gallery = document.querySelector("#gallery");
+let header = document.querySelector("#header");
+let footer = document.querySelector("#footer");
 let fullScreen = document.querySelector("#fullScreen");
 
 let titleName = document.querySelector("#titleName");
 let close = document.querySelector("#close");
 let fullScreenImage = document.querySelector("#fullScreenImage");
 
-galleryImages.forEach((images , index)=>{
-    images.addEventListener("click" , ()=>{
-        titleName.innerText = `Image No #${index + 1}`;
-        fullScreenImage.src = images.src;
-        gallery.style.display = "none";
-        fullScreen.style.display = "block";
-    });
+galleryImages.forEach((images, index) => {
+  images.addEventListener("click", () => {
+    titleName.innerText = `Image No #${index + 1}`;
+    fullScreenImage.src = images.src;
+    gallery.style.display = "none";
+    header.style.display = "none";
+    footer.style.display = "none";
+    fullScreen.style.display = "block";
+  });
 });
 
-close.addEventListener("click" , ()=>{
-    gallery.style.display = "grid";
-        fullScreen.style.display = "none";
+close.addEventListener("click", () => {
+  gallery.style.display = "grid";
+  header.style.display = "flex";
+  footer.style.display = "block";
+  fullScreen.style.display = "none";
 });
